fcac9fb123fc84970b44c9dd2c557f06251d05bb
Adrekx <ayush9931.mishra@gmail.com>
2025-03-11 21:17:26 +0530

    first commit

---
+++ src/main/java/com/fooddeliveryapplication/Zwiggato/ZwiggatoApplication.java
@@ -0,0 +1,13 @@
+package com.fooddeliveryapplication.Zwiggato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwiggatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZwiggatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fooddeliveryapplication/Zwiggato/ZwiggatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fooddeliveryapplication.Zwiggato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwiggatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
